0be814551a9b5bda9ad38635286a524ec7c14d9a
zyechu <matt96@wp.pl>
2020-12-07 16:19:35 +0100

    added getting data and datafile

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask
-from cepik_connector import get_voivodeship_dictionary, get_cars_dataframe
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def main_page():
-    voivodeships_dictionary = get_voivodeship_dictionary()
-    data = get_cars_dataframe(voivodeships_dictionary)
+    # voivodeships_dictionary = get_voivodeship_dictionary()
+    # data = get_cars_dataframe(voivodeships_dictionary)
+    return 0
 
 
 if __name__ == '__main__':
--- cepik_connector.py
+++ cepik_connector.py
@@ -10,6 +10,11 @@ import sqlite3
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
+data_start = "20190901"
+data_end = "20191231"
+
+#marka,model,rodzaj-pojazdu,podrodzaj-pojazdu,pochodzenie-pojazdu,sposob-produkcji,rok-produkcji,data-pierwszej-rejestracji-w-kraju,data-rejestracji-za-granica,pojemnosc-skokwa-silnika,moc-netto-silnika,masa-wlasna,liczba-miejsc-ogolem,rodzaj-paliwa,kierownica-po-prawej-stronie,rejestracja-wojewodztwo
+
 def get_voivodeship_dictionary():
     voivodeship_dict = dict()
     LINK = 'https://api.cepik.gov.pl/slowniki/wojewodztwa'
@@ -23,8 +28,10 @@ def get_voivodeship_dictionary():
 
 
 def get_max_pages(voivodship):
-    cepik_link = f"https://api.cepik.gov.pl/pojazdy?wojewodztwo={voivodship}&data-od=20200601&data-do=20201201&typ-daty=1" \
-                 f"&tylko-zarejestrowane=true&pokaz-wszystkie-pola=false&limit=500"
+    cepik_link = f"https://api.cepik.gov.pl/pojazdy?wojewodztwo={voivodship}&data-od={data_start}&data-do={data_end}&typ-daty=1" \
+                 f"&tylko-zarejestrowane=true&pokaz-wszystkie-pola=false&limit=300"\
+                 f"&filter[rodzaj-pojazdu]=samochód osobowy" \
+                 f"&fields=marka,model,rodzaj-pojazdu,podrodzaj-pojazdu,pochodzenie-pojazdu,sposob-produkcji,rok-produkcji,data-pierwszej-rejestracji-w-kraju,data-ostatniej-rejestracji-w-kraju,data-rejestracji-za-granica,pojemnosc-skokowa-silnika,moc-netto-silnika,masa-wlasna,liczba-miejsc-ogolem,rodzaj-paliwa,kierownica-po-prawej-stronie,rejestracja-wojewodztwo"
     response = requests.get(cepik_link, verify=False)
     response_json = response.json()
     parsed = urlparse.urlparse(response_json['links']['last'])
@@ -32,40 +39,56 @@ def get_max_pages(voivodship):
     return max_page
 
 
-
-
 def get_cars_dataframe(voivodeships):
-    df = pd.DataFrame()
+    # columns = ["marka","model,rodzaj-pojazdu","podrodzaj-pojazdu","pochodzenie-pojazdu","sposob-produkcji","rok-produkcji","data-pierwszej-rejestracji-w-kraju","data-ostatniej-rejestracji-w-kraju","data-rejestracji-za-granica","pojemnosc-skokowa-silnika","moc-netto-silnika","masa-wlasna","liczba-miejsc-ogolem","rodzaj-paliwa","kierownica-po-prawej-stronie","rejestracja-wojewodztwo"]
+
     page_int = 1
     print('Start voivodeships loop')
     for key in voivodeships:
+        df = pd.DataFrame()
         voivodship = voivodeships[key]
         last_page = get_max_pages(voivodship)
         print('downloading data for ', key)
         print('last page is ', last_page)
-        for i in range(1, last_page):
-            download_start = datetime.now()
+        for i in range(1, last_page + 1):
             print(f'Im on page number {i} of {last_page}')
             try:
-                cepik_link = f"https://api.cepik.gov.pl/pojazdy?wojewodztwo={voivodship}&data-od=20200601&data-do=20201201&typ-daty=1" \
-                             f"&tylko-zarejestrowane=true&pokaz-wszystkie-pola=true&limit=500&page={i}"
+                cepik_link = f"https://api.cepik.gov.pl/pojazdy?wojewodztwo={voivodship}&data-od={data_start}&data-do={data_end}&typ-daty=1" \
+                             f"&tylko-zarejestrowane=true&pokaz-wszystkie-pola=true&limit=300&page={i}"\
+                             f"&filter[rodzaj-pojazdu]=samochód osobowy"\
+                             f"&fields=marka,model,rodzaj-pojazdu,podrodzaj-pojazdu,pochodzenie-pojazdu," \
+                             f"sposob-produkcji,rok-produkcji,data-pierwszej-rejestracji-w-kraju," \
+                             f"data-ostatniej-rejestracji-w-kraju,data-rejestracji-za-granica," \
+                             f"pojemnosc-skokowa-silnika,moc-netto-silnika,masa-wlasna," \
+                             f"liczba-miejsc-ogolem,rodzaj-paliwa,kierownica-po-prawej-stronie,rejestracja-wojewodztwo"
+
                 response = requests.get(cepik_link, verify=False)
                 response_json = response.json()
                 response_json_data = response_json['data']
-                df.append(pd.json_normalize(response_json_data))
+                # print(response_json_data)
+
+                vehicle_tab = []
+                for vehicle in response_json_data:
+                    vehivle_dict = vehicle.get("attributes")
+                    vehicle_tab.append(vehivle_dict)
+                vehicle_dataframe = pd.DataFrame(vehicle_tab)
+                df = df.append(vehicle_dataframe, ignore_index=True)
+
+                # print(df)
             except:
                 print("Unexpected error:", sys.exc_info()[0])
                 pass
+        save_name = "woj_"+key
+        df.to_csv(save_name)
 
-    return df
+suma = 0
+v = get_voivodeship_dictionary()
+for key in v:
+    suma += get_max_pages(v[key])
+    print(f'{key} got pages: {get_max_pages(v[key])}')
 
-# suma = 0
-# v = get_voivodeship_dictionary()
-# for key in v:
-#     suma += get_max_pages(v[key])
-#     print(f'{key} got pages: {get_max_pages(v[key])}')
-#
-# print(suma)
+print(suma)
 
-# voivodeships_dictionary = get_voivodeship_dictionary()
-# data = get_cars_dataframe(voivodeships_dictionary)
+voivodeships_dictionary = get_voivodeship_dictionary()
+data = get_cars_dataframe(voivodeships_dictionary)
+print(data)
